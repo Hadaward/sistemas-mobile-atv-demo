e6a1434bdcc20345aeb16ac93bb153196d09de3e
Hadaward <eduardo.gimenez07@gmail.com>
2025-09-04 19:50:17 -0400

    chore(config): atualizar regras do eslint para melhor qualidade de código

--- eslint.config.js
+++ eslint.config.js
@@ -3,14 +3,29 @@ import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
-
+import baseDefault from '@phoenix35/eslint-config'
 export default tseslint.config(
-  { ignores: ['dist', 'cypress.config.ts'] },
+  {
+    ignores: [
+      'node_modules/**',
+      'dist/**',
+      'build/**',
+      'cypress.config.ts',
+      '**/dev/*',
+      '**/dist/*',
+      '**/tests/*',
+      'tsconfig.json',
+    ],
+  },
+  ...baseDefault,
+  ...tseslint.configs.strict,
+  ...tseslint.configs.stylistic,
   {
     extends: [js.configs.recommended, ...tseslint.configs.recommended],
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
+      sourceType: 'module',
       globals: globals.browser,
     },
     plugins: {
@@ -25,6 +40,17 @@ export default tseslint.config(
       ],
       'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
       'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+      // curly braces for all blocks
+      'curly': ['error', 'all'],
+      // double quotes
+      '@stylistic/js/quotes': ['error', 'double', { avoidEscape: true }],
+      // statement beside the control
+      '@stylistic/js/nonblock-statement-body-position': ['error', 'below'],
+      // disable camelcase rule
+      'camelcase': 'off',
+      // disable new-cap rule
+      'new-cap': 'off',
+      'no-undefined': 'off',
     },
   },
 )
